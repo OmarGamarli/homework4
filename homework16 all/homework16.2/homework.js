20fefbc4cc1dd98f2279ff1bfa55b07e95706e65
OmarGamarli <124809092+OmarGamarli@users.noreply.github.com>
2023-06-14 20:50:08 +0400

    Update homework.js

--- homework16 all/homework16.2/homework.js
+++ homework16 all/homework16.2/homework.js
@@ -31,12 +31,21 @@
 // first()
 
 // --------------------------4-----------------------
-// function change(){
-// ???
+function swap(str) {
+    return str.split('').map(function(char) {
+        if (char === char.toUpperCase()) {
+          return char.toLowerCase();
+        } else {
+          return char.toUpperCase();
+        }
+      })
+      .join('');
+  }
+  
+  let str = 'Hello  World!';
+  let swappedStr = swap(str);
+  console.log(swappedStr); 
 
-//    
-// }
-// change()
 
 // ----------------------------5------------------------------
 
